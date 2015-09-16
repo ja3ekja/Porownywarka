3b200b3c552ac3d2a9e8e278cfa3c5a6b448cec5
ja3ekja <jacekfeliksiak@gmail.com>
2015-09-16 11:41:57 +0200

    kolejny testowy komit

--- java/testowa.java
+++
@@ -1,9 +0,0 @@
-
-
-    public class Klasaa {
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-
---
+++ src/ClassWithMainMethod.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ja3ekja on 16.09.15.
+ */
+public class ClassWithMainMethod {
+    public static void main(String[] args){
+        System.out.println("Witaj świat! Powiedziała Maria po nocnej zmianie.");
+    }
+}
